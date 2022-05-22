6f06ab2e08f8a23bf469d42f0e57ae2dfed6b503
nneo <aisapple1030@naver.com>
2022-05-22 18:40:04 +0900

    jitter, shimmer 범위 수정

--- print_to_user.py
+++ print_to_user.py
@@ -1,50 +1,50 @@
-w_jitter_up_90 = 1.9996
-w_jitter_down_90 = 1.928
-w_shimmer_up_90 = 9.770
-w_shimmer_down_90 = 9.330
+w_jitter_up = 2.310
+w_jitter_down = 1.599
+w_shimmer_up = 12.221
+w_shimmer_down = 7.393
 
-m_jitter_up_90 = 2.610
-m_jitter_down_90 = 2.540
-m_shimmer_up_90 = 11.967
-m_shimmer_down_90 = 11.670
+m_jitter_up = 3.023
+m_jitter_down = 2.159
+m_shimmer_up = 13.526
+m_shimmer_down = 10.132
 
 def printWomenTone(shimmer, jitter):
     # shimmer
     shimmer_text = "※사용자님의 목소리 크기 변화율은 {:.3f}%로 크기 변화폭이 {} 편입니다.\n 여성의 평균적인 변화율은 {}% ~ {}%입니다.\n"
-    if shimmer<=w_shimmer_up_90 and w_shimmer_down_90<=shimmer:
-        print(shimmer_text.format(shimmer, "적당한", w_shimmer_down_90, w_shimmer_up_90)+"\n")
-    elif shimmer <= w_shimmer_down_90:
-        print(shimmer_text.format(shimmer, "작은",  w_shimmer_down_90, w_shimmer_up_90)+" 목소리 크기 변화가 적은 경우 좋은 내용이 지루하게 들릴 가능성이 있습니다.\n")
+    if shimmer<=w_shimmer_up and w_shimmer_down<=shimmer:
+        print(shimmer_text.format(shimmer, "적당한", w_shimmer_down, w_shimmer_up)+"\n")
+    elif shimmer <= w_shimmer_down:
+        print(shimmer_text.format(shimmer, "작은",  w_shimmer_down, w_shimmer_up)+" 목소리 크기 변화가 적은 경우 좋은 내용이 지루하게 들릴 가능성이 있습니다.\n")
     else:
-        print(shimmer_text.format(shimmer, "큰", w_shimmer_down_90, w_shimmer_up_90)+" 목소리 크기 변화가 과도하게 큰 경우 오히려 전달력이 떨어질 가능성이 있습니다.\n")
+        print(shimmer_text.format(shimmer, "큰", w_shimmer_down, w_shimmer_up)+" 목소리 크기 변화가 과도하게 큰 경우 오히려 전달력이 떨어질 가능성이 있습니다.\n")
     
     # jitter
     jitter_text = "※사용자님의 목소리 높낮이 변화율은 {:.3f}%으로 높낮이 변화폭이 {} 편입니다.\n 여성의 평균적인 변화율은 {}% ~ {}%입니다.\n"
-    if jitter<=w_jitter_up_90 and w_jitter_down_90<=jitter:
-        print(jitter_text.format(jitter, "적당한", w_jitter_down_90, w_jitter_up_90)+"\n")
-    elif jitter <= w_jitter_down_90:
-        print(jitter_text.format(jitter, "작은",  w_jitter_down_90, w_jitter_up_90)+" 목소리 크기 변화가 적은 경우 좋은 내용이 지루하게 들릴 가능성이 있습니다.\n")
+    if jitter<=w_jitter_up and w_jitter_down<=jitter:
+        print(jitter_text.format(jitter, "적당한", w_jitter_down, w_jitter_up)+"\n")
+    elif jitter <= w_jitter_down:
+        print(jitter_text.format(jitter, "작은",  w_jitter_down, w_jitter_up)+" 목소리 크기 변화가 적은 경우 좋은 내용이 지루하게 들릴 가능성이 있습니다.\n")
     else:
-        print(jitter_text.format(jitter, "큰", w_jitter_down_90, w_jitter_up_90)+" 목소리 크기 변화가 과도하게 큰 경우 오히려 전달력이 떨어질 가능성이 있습니다.\n")
+        print(jitter_text.format(jitter, "큰", w_jitter_down, w_jitter_up)+" 목소리 크기 변화가 과도하게 큰 경우 오히려 전달력이 떨어질 가능성이 있습니다.\n")
 
 def printMenTone(shimmer, jitter):
     # shimmer
     shimmer_text = "※사용자님의 목소리 크기 변화율은 {:.3f}%로 크기 변화폭이 {} 편입니다.\n남성의 평균적인 변화율은 {}% ~ {}%입니다.\n"
-    if shimmer<=m_shimmer_up_90 and m_shimmer_down_90<=shimmer:
-        print(shimmer_text.format(shimmer, "적당한", m_shimmer_down_90, m_shimmer_up_90)+"\n")
-    elif shimmer <= m_shimmer_down_90:
-        print(shimmer_text.format(shimmer, "작은",  m_shimmer_down_90, m_shimmer_up_90)+" 목소리 크기 변화가 적은 경우 좋은 내용이 지루하게 들릴 가능성이 있습니다.\n")
+    if shimmer<=m_shimmer_up and m_shimmer_down<=shimmer:
+        print(shimmer_text.format(shimmer, "적당한", m_shimmer_down, m_shimmer_up)+"\n")
+    elif shimmer <= m_shimmer_down:
+        print(shimmer_text.format(shimmer, "작은",  m_shimmer_down, m_shimmer_up)+" 목소리 크기 변화가 적은 경우 좋은 내용이 지루하게 들릴 가능성이 있습니다.\n")
     else:
-        print(shimmer_text.format(shimmer, "큰", m_shimmer_down_90, m_shimmer_up_90)+" 목소리 크기 변화가 과도하게 큰 경우 오히려 전달력이 떨어질 가능성이 있습니다.\n")
+        print(shimmer_text.format(shimmer, "큰", m_shimmer_down, m_shimmer_up)+" 목소리 크기 변화가 과도하게 큰 경우 오히려 전달력이 떨어질 가능성이 있습니다.\n")
     
     # jitter
     jitter_text = "※사용자님의 목소리 높낮이 변화율은 {:.3f}%으로 높낮이 변화폭이 {} 편입니다.\n남성의 평균적인 변화율은 {}% ~ {}%입니다.\n"
-    if jitter<=m_jitter_up_90 and m_jitter_down_90<=jitter:
-        print(jitter_text.format(jitter, "적당한", m_jitter_down_90, m_jitter_up_90)+"\n")
-    elif jitter <= m_jitter_down_90:
-        print(jitter_text.format(jitter, "작은",  m_jitter_down_90, m_jitter_up_90)+" 목소리 크기 변화가 적은 경우 좋은 내용이 지루하게 들릴 가능성이 있습니다.\n")
+    if jitter<=m_jitter_up and m_jitter_down<=jitter:
+        print(jitter_text.format(jitter, "적당한", m_jitter_down, m_jitter_up)+"\n")
+    elif jitter <= m_jitter_down:
+        print(jitter_text.format(jitter, "작은",  m_jitter_down, m_jitter_up)+" 목소리 크기 변화가 적은 경우 좋은 내용이 지루하게 들릴 가능성이 있습니다.\n")
     else:
-        print(jitter_text.format(jitter, "큰", m_jitter_down_90, m_jitter_up_90)+" 목소리 크기 변화가 과도하게 큰 경우 오히려 전달력이 떨어질 가능성이 있습니다.\n")
+        print(jitter_text.format(jitter, "큰", m_jitter_down, m_jitter_up)+" 목소리 크기 변화가 과도하게 큰 경우 오히려 전달력이 떨어질 가능성이 있습니다.\n")
 
 def printTone(shimmer, jitter, gender):
     if gender == "W":
